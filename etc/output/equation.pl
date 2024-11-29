c7125430396b7c9036eca1ee6d04ff4870115522
josd <josderoo@gmail.com>
2024-11-30 00:21:33 +0100

    refreshing

---
+++ etc/output/equation.pl
@@ -0,0 +1,5 @@
+:- op(1150, xfx, :+).
+
+'http://eyereasoner.github.io/etc#equation'(x^2-3*x+2=0, x, [2.0, 1.0]):+true.
+'http://eyereasoner.github.io/etc#equation'(cos(x)*(1-2*sin(x))=0, x, [1.5707963267948966, -1.5707963267948966, -1.5707963267948966, 0.5235987755982989, 179.4764012244017]):+true.
+'http://eyereasoner.github.io/etc#equation'(2^(2*x)-5*2^(x+1)+16=0, x, [3.0, 1.0]):+true.
